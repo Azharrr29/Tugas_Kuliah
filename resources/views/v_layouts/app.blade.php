341347821dec6716bedc89d30581ddfe43dd420d
Azharrr29 <azharzaidan420@gmail.com>
2025-05-08 09:25:14 +0700

    Pertemuan 1

--- resources/views/v_layouts/app.blade.php
+++ resources/views/v_layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('image/icon_univ_bsi.png') }}">
-    <title>eshop</title>
+    <title>Eshop</title>
 
     <!-- Google font -->
     <link href="https://fonts.googleapis.com/css?family=Hind:400,700" rel="stylesheet">
